1341208fd8b5047ac67e148c7746b9416fede7c6
dayrain <1376034301@qq.com>
2024-02-20 21:29:56 +0800

    project init

---
+++ wms-api/src/main/java/com/dayrain/wms/api/DddWmsApplication.java
@@ -0,0 +1,13 @@
+package com.dayrain.wms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DddWmsApplication.class, args);
+    }
+
+}
